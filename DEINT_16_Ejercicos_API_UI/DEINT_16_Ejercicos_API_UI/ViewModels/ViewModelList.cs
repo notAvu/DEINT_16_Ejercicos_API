f5f519ee32caa57f2d70751d82b6abe9b8fe625e
Álvaro Fernández <avunotfound@gmail.com>
2022-01-18 11:05:41 +0100

    No entendio la referencia

---
+++ DEINT_16_Ejercicos_API_UI/DEINT_16_Ejercicos_API_UI/ViewModels/ViewModelList.cs
@@ -0,0 +1,13 @@
+﻿//using DEINT_16_Ejercios_API_Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DEINT_16_Ejercicos_API_UI.ViewModels
+{
+    public class ViewModelList
+    {
+        //List<clsPersona> listadoPersonas;
+        public void RellenarLista() { }
+    }
+}
